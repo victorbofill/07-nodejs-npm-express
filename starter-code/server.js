fec1670c3cfb90dad11e495b9a7256661476d7e9
Victor Bofill <terranvas@gmail.com>
2018-03-06 14:04:03 -0800

    Tentative completion of server.js TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -9,17 +9,27 @@ const PORT = process.env.PORT || 3000; /* eslint-disable-line */ // TODO: remove
 // TODONE: use express.static to server the public path!
 app.use(express.static('public'));
 
-// TODO: server new.html under the alias GET /new
+// TODONE: server new.html under the alias GET /new
 // (HINT: use response.sendFile)
+app.get('/new', (request, response) => {
+  response.sendFile('/new.html');
+});
 
-// TODO: add a app.get for `/api/articles` that returns the
+// TODONE: add a app.get for `/api/articles` that returns the
 // `data/hackerIpsum.json`
+app.get('/api/articles', (request, response) => {
+  response.sendFile(`${__dirname}/data/hackerIpsum.json`);
+});
 
-// TODO: server your articles data on GET /api/articles
+// TODONE: server your articles data on GET /api/articles
 app.post('/api/articles', bodyParser, (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
-
+  const file = 'data/hackerIpsum.json';
+  const raw = fs.readFileSync(file);
+  const articles = JSON.parse(raw);
+  articles.push(request.body);
+  fs.writeFileSync(file, JSON.stringify(articles, true, 2));
   // for now just return the body...
   response.send(request.body);
 
